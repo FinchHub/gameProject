a6780786d4a7a37463ed1bd734d70034f94b3789
Chance Crosson <chancecrosson@gmail.com>
2025-10-05 11:33:51 -0500

    Added empty functions and initalized ints in makeBoard().

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <vector>
+
+// Board will be 16x16 with 4x4 
+void makeBoard(){
+    const int BOARD_LENGTH = 16;
+    const int BOARD_SIZE = BOARD_LENGTH * BOARD_LENGTH;
+    char board[BOARD_LENGTH][BOARD_LENGTH];
+    
+
+}
+void play(){ }
+void gameStatus(){ }
+
 
 
 int main() {
