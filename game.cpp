6059c3d9e559bc5be348fa60aa39673a21f1dded
Chance <chancecrosson@gmail.com>
2025-10-07 15:27:19 -0500

    Empty functions added. To-do added

---
+++ game.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <random>
+
+enum GameStatus {
+    ONGOING,
+    P1_WINS,
+    P2_WINS,
+    DRAW,
+};
+
+void placePlayers() {
+
+
+}
+
+void startGame() {
+
+}
+
+
+
+//To-do:
+//
+//1. Place players, print the board.
+//2. Movement for direction.
+//3. Chip collection
+//4. Full sliding in all directions
+//5. Turn logic/Scoring
+//6. GameState detection? GameStatus updates
