c7fed29706e42fac97c6ceb3c6c1ead543db910b
Chance <chancecrosson@gmail.com>
2025-10-07 15:03:38 -0500

    Added two gameboards for testing.

--- board.cpp
+++ board.cpp
@@ -2,9 +2,8 @@
 #include <vector>
 #include <random>
 
-// build obstacles, place players, and change gamestate?
 std::vector<std::vector<char>> makeBoard(){
-    std::vector<std::vector<char>> board(16, std::vector<char>(16, '.'));   
+    std::vector<std::vector<char>> board(8, std::vector<char>(8, '.'));   
     
     //Rand logic pulled from stackoverflow. First time using it.
     //https://stackoverflow.com/questions/12657962/how-do-i-generate-a-random-number-between-two-variables-that-i-have-stored
@@ -12,24 +11,55 @@ std::vector<std::vector<char>> makeBoard(){
     std::mt19937 engine(seeder());
     std::uniform_int_distribution<int> dist(1,2);
 
-    //Default obstacle. Do not modify
-    board[7][7] = '#';
-    board[7][8] = '#';
-    board[8][7] = '#';
-    board[8][8] = '#';
+    //Randomly generating a map and ensuring it's solvable is way beyond my skill level.
+    //As much of a pain it is to handmake a solvable map, it's the only alternative I can think of.
+    //Better implementation suggestions would be very much appreciated!
 
-    //Using two presets in this stage because randomly generating them is going to be hellish at my skill level
     //Board preset 1
     auto board1 = board;
-    board1[1][1] = '#';
-    board1[1][2] = '#';
-    board1[2][1] = '#';
+    int b1walls[][2] = {
+        {0, 2}, {0, 6},
+        {1, 2}, {1, 5},
+        {2, 4},
+        {3, 0}, {3, 3}, {3, 4},
+        {4, 2}, {4, 3}, {4, 4}, {4, 6},
+        {5, 1}, {5, 5},
+        {7, 4}
+    };
+
+    int b1chips[][2] = {
+        {6, 2},
+        {6, 5},
+        {2, 7},
+        {4, 0}
+    };
+    
+    for (auto& w : b1walls) board1[w[0]][w[1]] = '#';
+    for (auto& c : b1chips) board1[c[0]][c[1]] = 'C';
 
     //Board preset 2
     auto board2 = board;
-    board2[10][10] = '#';
-    board2[9][10] = '#';
-    board2[10][9] = '#';
+    int b2walls[][2] = {
+        //{0, 4},
+        {1, 1}, {1, 5},
+        {2, 0}, {2, 3}, {2, 7},
+        {3, 2}, {3, 3},
+        {4, 1}, {4, 5}, {4, 6},
+        {5, 3},
+        {6, 6},
+        {7, 0}
+    };
+
+    // Chips
+    int b2chips[][2] = {
+        {6, 1},
+        {6, 4},
+        {1, 6},
+        {4, 2}
+    };
+
+    for (auto& w : b2walls) board2[w[0]][w[1]] = '#';
+    for (auto& c : b2chips) board2[c[0]][c[1]] = 'C';
 
     int randSel = dist(engine);
 
@@ -41,8 +71,8 @@ std::vector<std::vector<char>> makeBoard(){
 }
 
 void printBoard(std::vector<std::vector<char>>& board){
-   for (int row = 0; row<16; row++) {
-        for (int col = 0; col < 16; col++){
+   for (int row = 0; row<8; row++) {
+        for (int col = 0; col < 8; col++){
             std::cout << board[row][col] << " ";
         }
         std::cout << "\n";
